fcaf9e4729c48c20cde2c6f8e4f10e49810c358e
Daniel Suarez <0321daniels@gmail.com>
2025-06-14 07:05:25 -0500

    feat: update admin layout and navbar styles, remove unused club component files

--- src/app/domains/admin/components/navbar/navbar.component.ts
+++ src/app/domains/admin/components/navbar/navbar.component.ts
@@ -4,7 +4,7 @@ import { RouterLink, RouterLinkActive } from '@angular/router';
 import {OverlayModule} from '@angular/cdk/overlay';
 
 @Component({
-  selector: 'app-navbar',
+  selector: 'app-navbar-admin',
   standalone: true,
   imports: [CommonModule, RouterLink, RouterLinkActive, OverlayModule],
   templateUrl: './navbar.component.html',
@@ -12,29 +12,20 @@ import {OverlayModule} from '@angular/cdk/overlay';
 export class NavbarComponent {
   isOpenAccount = false;
   isOpenMenuMovil = false;
-  isOpeninventario = false;
 
   openMenus(overlay: string){
     if(overlay === 'account') {
       this.isOpenAccount = !this.isOpenAccount; 
       this.isOpenMenuMovil = false; 
-      this.isOpeninventario = false;
     }
     if(overlay === 'menuMovil'){
       this.isOpenMenuMovil = !this.isOpenMenuMovil; 
       this.isOpenAccount = false; 
-      this.isOpeninventario = false;
-    } 
-    if(overlay === 'inventario'){
-      this.isOpeninventario = !this.isOpeninventario; 
-      this.isOpenAccount = false; 
-      this.isOpenMenuMovil = false;
     } 
   }
 
   cerrarMenus(){
-    if(this.isOpeninventario || this.isOpenMenuMovil || this.isOpenAccount){
-      this.isOpeninventario = false;
+    if(this.isOpenMenuMovil || this.isOpenAccount){
       this.isOpenMenuMovil = false;
       this.isOpenAccount = false;
     }
