24d4535df5d97e6775c302ffb62394e3ae068b87
Daniel Suarez <0321daniels@gmail.com>
2025-06-14 20:12:26 -0500

    feat: add environment configuration files for production, quality, and server development; update service URLs and cookie tokens

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,7 @@
+export const environment = {
+  production: true,
+  urlServices: 'https://api.tiaras.top/tiaras/api/v1/',
+  nombreCookieToken: 'tiaras_colombia',
+  duracionMinutosCookieToken: 1440,
+  urlAplicacion: 'https://tiaras.pages.dev'
+};
