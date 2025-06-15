f7c55401a918c2b30b6d5c1bdf75fd6b23f553a3
Daniel Suarez <0321daniels@gmail.com>
2025-06-15 18:16:32 -0500

    fix: update urlServices to use HTTPS for production environment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  urlServices: 'http://ec2-18-212-24-228.compute-1.amazonaws.com:8080/',
+  urlServices: 'https://ec2-18-212-24-228.compute-1.amazonaws.com:8080/',
   nombreCookieToken: 'sportsclubmanager_token',
   duracionMinutosCookieToken: 1440,
   urlAplicacion: 'https://sportsclubmanager.pages.dev/'
