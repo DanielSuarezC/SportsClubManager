7c75a1fe82e8b51b810ab16b158fc0d44926dcf5
Daniel Suarez <0321daniels@gmail.com>
2025-06-21 07:12:45 -0500

    feat: add bcrypt and crypto-js libraries, update environment URLs for production and development

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  urlServices: 'https://ec2-18-212-24-228.compute-1.amazonaws.com:8080/',
+  urlServices: 'https://jsonserver-fjiq.onrender.com/',
   nombreCookieToken: 'sportsclubmanager_token',
   duracionMinutosCookieToken: 1440,
   urlAplicacion: 'https://sportsclubmanager.pages.dev/'
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,10 +4,10 @@
 
 export const environment = {
   production: true,
-  urlServices: 'http://ec2-18-212-24-228.compute-1.amazonaws.com:8080/',
+  urlServices: 'http/localhost:3000/',
   nombreCookieToken: 'sportsclubmanager_token',
   duracionMinutosCookieToken: 60,
-  urlAplicacion: 'http://localhost:4200/'
+  urlAplicacion: 'http://localhost:4200'
 };
 
 /*
