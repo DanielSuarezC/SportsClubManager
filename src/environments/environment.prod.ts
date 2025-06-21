d71140e7c743fb96fae3fa9448fa0ba52117da0d
Daniel Suarez <0321daniels@gmail.com>
2025-06-21 08:02:25 -0500

    fix: Remove trailing slashes from production environment URLs

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
-  urlServices: 'https://jsonserver-fjiq.onrender.com/',
+  urlServices: 'https://jsonserver-fjiq.onrender.com',
   nombreCookieToken: 'sportsclubmanager_token',
   duracionMinutosCookieToken: 1440,
-  urlAplicacion: 'https://sportsclubmanager.pages.dev/'
+  urlAplicacion: 'https://sportsclubmanager.pages.dev'
 };
